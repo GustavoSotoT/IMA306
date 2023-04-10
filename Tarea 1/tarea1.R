e0f50d3996e2725d67e431bd50916503d0dd734b
Gustavo <gustavosotot06@gmail.com>
2023-04-10 10:19:28 -0400

    first commit

---
+++ Tarea 1/tarea1.R
@@ -0,0 +1,21 @@
+getwd()
+datos1=read.table("C:/Users/gsoto/Desktop/U/Series de tiempo/Tarea 1/data1.txt")
+Box.test(datos1)
+
+
+
+datos2=read.table("C:/Users/gsoto/Desktop/U/Series de tiempo/Tarea 1/data2.txt")
+Box.test(datos2)
+
+
+datos3=read.table("C:/Users/gsoto/Desktop/U/Series de tiempo/Tarea 1/data3.txt")
+Box.test(datos3)
+
+datos4=read.table("C:/Users/gsoto/Desktop/U/Series de tiempo/Tarea 1/data4.txt")
+Box.test(datos4)
+
+
+datos5=read.table("C:/Users/gsoto/Desktop/U/Series de tiempo/Tarea 1/data5.txt")
+Box.test(datos5)
+
+
